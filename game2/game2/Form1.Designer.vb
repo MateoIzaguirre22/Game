b07ca07ea4cde1ed8fe8e596151d85f0f47be2a8
MateoIzaguirre22 <39205126+MateoIzaguirre22@users.noreply.github.com>
2018-08-10 22:50:25 -0300

    subiendo codigo

---
+++ game2/game2/Form1.Designer.vb
@@ -0,0 +1,226 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.movimiento = New System.Windows.Forms.Timer(Me.components)
+        Me.gravedad = New System.Windows.Forms.Timer(Me.components)
+        Me.caida = New System.Windows.Forms.Timer(Me.components)
+        Me.Animacion = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.vida2 = New System.Windows.Forms.Panel()
+        Me.vida1 = New System.Windows.Forms.Panel()
+        Me.poder = New System.Windows.Forms.Panel()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Panel6 = New System.Windows.Forms.Panel()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'movimiento
+        '
+        Me.movimiento.Interval = 1
+        '
+        'gravedad
+        '
+        Me.gravedad.Interval = 1
+        '
+        'caida
+        '
+        Me.caida.Interval = 1
+        '
+        'Animacion
+        '
+        Me.Animacion.Enabled = True
+        Me.Animacion.Interval = 150
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Panel1.Location = New System.Drawing.Point(4, 357)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(854, 100)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Panel2.Location = New System.Drawing.Point(765, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(93, 369)
+        Me.Panel2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(117, 74)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "animacion"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(178, 74)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Label2"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'vida2
+        '
+        Me.vida2.BackColor = System.Drawing.Color.Red
+        Me.vida2.Location = New System.Drawing.Point(84, 12)
+        Me.vida2.Name = "vida2"
+        Me.vida2.Size = New System.Drawing.Size(200, 32)
+        Me.vida2.TabIndex = 9
+        '
+        'vida1
+        '
+        Me.vida1.BackColor = System.Drawing.Color.Lime
+        Me.vida1.Location = New System.Drawing.Point(84, 12)
+        Me.vida1.Name = "vida1"
+        Me.vida1.Size = New System.Drawing.Size(200, 32)
+        Me.vida1.TabIndex = 10
+        '
+        'poder
+        '
+        Me.poder.BackColor = System.Drawing.Color.Cyan
+        Me.poder.Location = New System.Drawing.Point(86, 50)
+        Me.poder.Name = "poder"
+        Me.poder.Size = New System.Drawing.Size(200, 11)
+        Me.poder.TabIndex = 11
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(583, 150)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(137, 198)
+        Me.PictureBox2.TabIndex = 10
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = Global.game2.My.Resources.Resources.snap6
+        Me.PictureBox1.Location = New System.Drawing.Point(135, 150)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(120, 201)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Panel3.Location = New System.Drawing.Point(4, 0)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(74, 419)
+        Me.Panel3.TabIndex = 2
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.Lime
+        Me.Panel4.Location = New System.Drawing.Point(559, 12)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(200, 32)
+        Me.Panel4.TabIndex = 13
+        '
+        'Panel5
+        '
+        Me.Panel5.BackColor = System.Drawing.Color.Cyan
+        Me.Panel5.Location = New System.Drawing.Point(561, 50)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(200, 11)
+        Me.Panel5.TabIndex = 14
+        '
+        'Panel6
+        '
+        Me.Panel6.BackColor = System.Drawing.Color.Red
+        Me.Panel6.Location = New System.Drawing.Point(559, 12)
+        Me.Panel6.Name = "Panel6"
+        Me.Panel6.Size = New System.Drawing.Size(200, 32)
+        Me.Panel6.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(858, 491)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel5)
+        Me.Controls.Add(Me.Panel6)
+        Me.Controls.Add(Me.vida1)
+        Me.Controls.Add(Me.poder)
+        Me.Controls.Add(Me.vida2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents movimiento As System.Windows.Forms.Timer
+    Friend WithEvents gravedad As System.Windows.Forms.Timer
+    Friend WithEvents caida As System.Windows.Forms.Timer
+    Friend WithEvents Animacion As System.Windows.Forms.Timer
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents vida2 As System.Windows.Forms.Panel
+    Friend WithEvents vida1 As System.Windows.Forms.Panel
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents poder As System.Windows.Forms.Panel
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents Panel4 As System.Windows.Forms.Panel
+    Friend WithEvents Panel5 As System.Windows.Forms.Panel
+    Friend WithEvents Panel6 As System.Windows.Forms.Panel
+
+End Class
---
+++ game2/game2/Form1.vb
@@ -0,0 +1,246 @@
+﻿Public Class Form1
+    Dim contador As Integer
+    Dim caminar1 As Integer = 0
+    Dim caminar2 As Integer = 0
+    Dim animacion_quieto As Integer
+    Dim animacion_caminar As Integer
+    Dim salto As Integer
+    Dim pared As Integer
+    Dim desplazamiento As Integer
+    Dim vida As Integer = 200
+    Dim power As Integer = 100
+    Dim ataque As Integer
+
+
+
+
+
+
+    Private Sub Form1_KeyDown1(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.Right Then
+            caminar1 = 1
+            movimiento.Enabled = True
+            animacion_quieto = 0
+            animacion_caminar = 1
+        End If
+        If e.KeyCode = Keys.Up Then
+            salto = 1
+            animacion_quieto = 0
+            animacion_caminar = 0
+            gravedad.Enabled = True
+
+        End If
+        If e.KeyCode = Keys.Left Then
+            caminar2 = 1
+            animacion_quieto = 0
+            animacion_caminar = 2
+            movimiento.Enabled = True
+        End If
+        If e.KeyCode = Keys.A Then
+            ataque = 1
+        End If
+        If e.KeyCode = Keys.S Then
+            power = power + 1
+        End If
+    End Sub
+
+    Private Sub Form1_KeyUp1(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyUp
+        If e.KeyCode = Keys.Right Then
+            caminar1 = 0
+            animacion_quieto = 1
+            animacion_caminar = 0
+        End If
+
+        If e.KeyCode = Keys.Left Then
+            caminar2 = 0
+            animacion_quieto = 1
+            animacion_caminar = 0
+        End If
+        If e.KeyCode = Keys.A Then
+            ataque = 0
+            PictureBox2.Dispose()
+        End If
+
+        
+    End Sub
+
+
+
+
+
+
+
+
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles movimiento.Tick
+        If caminar1 = 1 Then
+            PictureBox1.Location = New Point(PictureBox1.Location.X + 1, PictureBox1.Location.Y)
+        End If
+        If caminar2 = 1 Then
+            PictureBox1.Location = New Point(PictureBox1.Location.X - 1, PictureBox1.Location.Y)
+        End If
+
+        If PictureBox1.Location.X = Panel2.Location.X - 50 Then
+            PictureBox1.Location = New Point(PictureBox1.Location.X - 1, PictureBox1.Location.Y)
+        End If
+
+
+    End Sub
+
+
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles gravedad.Tick
+
+
+        If salto = 1 Then
+
+            PictureBox1.Location = New Point(PictureBox1.Location.X, PictureBox1.Location.Y - 1)
+
+        End If
+        If PictureBox1.Location.Y = 100 Then
+            salto = 0
+            gravedad.Enabled = False
+            caida.Enabled = True
+
+        End If
+
+
+
+    End Sub
+
+
+    Private Sub Timer3_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles caida.Tick
+        desplazamiento = desplazamiento + 1
+        PictureBox1.Location = New Point(PictureBox1.Location.X, PictureBox1.Location.Y + 1)
+        If PictureBox1.Location.Y = Panel1.Location.Y - 200 Then
+            caida.Enabled = False
+            animacion_quieto = 1
+
+        End If
+
+    End Sub
+
+    Private Sub Timer4_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Animacion.Tick
+        animacion_quieto = 1
+
+        If PictureBox2.Location.X < PictureBox1.Location.X Then
+            PictureBox2.Image = My.Resources.snap1
+        Else
+            PictureBox2.Image = My.Resources.snap1
+            PictureBox2.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+        End If
+
+
+        If PictureBox2.Location.X > PictureBox1.Location.X Then
+            PictureBox1.Image = My.Resources.snap1
+        Else
+            animacion_quieto = 2
+        End If
+
+
+
+
+
+
+        Label2.Text = contador
+        If animacion_quieto = 1 Then
+            contador = contador + 1
+            If contador >= 3 Then
+                contador = contador * 0
+            End If
+            If contador = 1 Then
+                PictureBox1.Image = My.Resources.snap1
+            End If
+            If contador = 2 Then
+                PictureBox1.Image = My.Resources.snap2
+            End If
+
+        End If
+
+        If animacion_quieto = 2 Then
+            contador = contador + 1
+            If contador >= 3 Then
+                contador = contador * 0
+            End If
+            If contador = 1 Then
+                PictureBox1.Image = My.Resources.snap1
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+            End If
+            If contador = 2 Then
+                PictureBox1.Image = My.Resources.snap2
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+            End If
+
+        End If
+
+
+
+
+
+        If animacion_caminar = 1 Then
+            contador = contador + 1
+            If contador >= 5 Then
+                contador = contador * 0
+            End If
+            If contador = 0 Then
+                PictureBox1.Image = My.Resources.snap3
+            End If
+            If contador = 1 Then
+                PictureBox1.Image = My.Resources.snap4
+            End If
+            If contador = 2 Then
+                PictureBox1.Image = My.Resources.snap5
+            End If
+            If contador = 3 Then
+                PictureBox1.Image = My.Resources.snap6
+            End If
+        End If
+
+        If animacion_caminar = 2 Then
+            contador = contador + 1
+
+            If contador >= 5 Then
+                contador = contador * 0
+            End If
+            If contador = 0 Then
+                PictureBox1.Image = My.Resources.snap3
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+            End If
+            If contador = 1 Then
+                PictureBox1.Image = My.Resources.snap4
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+            End If
+            If contador = 2 Then
+                PictureBox1.Image = My.Resources.snap5
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+            End If
+            If contador = 3 Then
+                PictureBox1.Image = My.Resources.snap6
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+            End If
+        End If
+
+
+    End Sub
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        PictureBox2.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+
+    End Sub
+
+   
+    Private Sub Timer1_Tick_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        vida1.Width = vida
+        poder.Width = power
+        If PictureBox1.Location.X = PictureBox2.Location.X Then
+            vida = vida - 26
+        End If
+        If ataque = 1 Then
+            power = power - 10
+        End If
+    End Sub
+
+
+   
+End Class
